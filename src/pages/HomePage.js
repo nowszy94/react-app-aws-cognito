de9dda4bb05ab3f0b0b55de59b259c52f1ecf09e
Szymon Nowak <szymon.nowak@allegro.pl>
2020-03-15 00:02:50 +0100

    created user verification

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ const validatePassword = (password, confirmPassword) => {
     if (password !== confirmPassword) {
         return 'PasswordsNotEqual';
     }
-    if (!/\d/.text(password)) {
+    if (!/\d/.test(password)) {
         return 'MissingNumberInPassword';
     }
     // TODO add full validation (has lowercase/uppercase letter)
@@ -22,14 +22,16 @@ const errorCodesMessages = {
 const translateErrorCode = (errorCode) => errorCodesMessages[errorCode] || 'Nieznany blad';
 
 export default () => {
+    const [user, setUser] = useState(null);
     const [email, setEmail] = useState('nowszy94@gmail.com');
     const [password, setPassword] = useState('Test1234');
     const [confirmPassword, setConfirmPassword] = useState('Test1234');
+    const [verificationCode, setVerificationCode] = useState('');
     const [success, setSuccess] = useState(false);
     const [error, setError] = useState(false);
     const [errorCode, setErrorCode] = useState(null);
 
-    const handleSubmit = (event) => {
+    const handleRegisterSubmit = (event) => {
         event.preventDefault();
         const passwordValidation = validatePassword(password, confirmPassword);
 
@@ -48,15 +50,28 @@ export default () => {
                     console.log('Success', data);
                     setSuccess(true);
                     setError(false);
+                    setUser(data.user)
                 }
             });
         }
     };
 
+    const handleVerificationSubmit = (event) => {
+        event.preventDefault();
+
+        user.confirmRegistration(verificationCode, false, (err, data) => {
+            if (err) {
+                console.error('Error while confirm account', err)
+            } else {
+                console.log('Success', data);
+            }
+        });
+    };
+
     return <div>
         <div>
-            <h2>Register</h2>
-            <form onSubmit={handleSubmit}>
+            <h2>{!success ? 'Register' : 'Confirm Registration'}</h2>
+            {!success && <form onSubmit={handleRegisterSubmit}>
                 Email: <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} /> <br />
                 Password: <input
                     type="password"
@@ -71,7 +86,11 @@ export default () => {
                     onChange={(event) => setConfirmPassword(event.target.value)}
             /> <br />
                 <button type="submit">Register</button>
-            </form>
+            </form>}
+            {success && <form onSubmit={handleVerificationSubmit}>
+                Code: <input type="text" value={verificationCode} onChange={(event) => setVerificationCode(event.target.value)} /> <br />
+                <button type="submit">Submit</button>
+            </form>}
             {error && <p className={styles.errorMessage}>Error: {translateErrorCode(errorCode)}</p>}
         </div>
     </div>;
